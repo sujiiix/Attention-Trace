a9e97f768a4cae6b06743c734dd45330169007c3
sujiiix <sujanchaurasia22@gmail.com>
2026-08-14 13:41:02 +0530

    Basic Change

--- backend/main.py
+++ backend/main.py
@@ -799,4 +799,4 @@ def send_contact_message(contact: ContactMessage, current_user_id: str = Depends
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
